f25d26ea70527a472307e8070c3ff560372fd1a1
Allison Lamb <allison.lamb624@gmail.com>
2023-12-11 22:40:47 -0500

    shopping form made

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import './App.css'; 
+import React, {useState, useEffect} from 'react'; 
+
+ 
+import ShoppingForm from './Components/ShoppingForm/ShoppingForm'; 
+import ShoppingList from './Components/ShoppingList/ShoppingList'; 
 
  
 function App() { 
+  const [shoppingList, setShoppingList] = useState([]); 
+
+ 
+  const loadData = () => { 
+    fetch('https://rysgws-8080.csb.app/api/list') 
+      .then(x => x.json()) 
+      .then(response => { 
+        setShoppingList(response); 
+      }); 
+  }; 
+
+ 
+  useEffect(loadData, []); 
+
+ 
+  const addItem = (item, quantity) => { 
+    fetch("https://rysgws-8080.csb.app/api/list/new", { 
+      method: "POST", 
+      body: JSON.stringify({ 
+        item, 
+        quantity 
+      }), 
+      headers: { 
+        "Content-type": "application/json; charset=UTF-8", 
+      }, 
+      mode: "cors" 
+    }) 
+      .then(x => x.json()) 
+      .then(loadData); 
+  } 
+ 
+  const deleteItem = (id) => { 
+    fetch("https://rysgws-8080.csb.app/api/list/" + id, { 
+        method: "DELETE", 
+        headers: { 
+            "Content-type": "application/json; charset=UTF-8", 
+        }, 
+        mode: "cors", 
+    }) 
+        .then((x) => x.json()) 
+        .then(loadData); 
+}; 
+ 
   return ( 
     <div className="App"> 
       <header className="App-header"> 
         <h1>Shopping List</h1> 
       </header> 
-      <main></main> 
+      <main> 
+        <ShoppingForm addItem={addItem} /> 
+        <ShoppingList items={shoppingList}  deleteItem={deleteItem}/> 
+      </main> 
     </div> 
   ); 
 } 
- 
+
+
 export default App; 
---
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,37 @@
+import React from 'react'; 
+
+function ShoppingItem({ id, item, quantity, deleteItem }) { 
+    function handleDelete(event) { 
+        event.preventDefault(); 
+        deleteItem(id); 
+    } 
+
+    function handleEdit(event) {
+        event.preventDefault();
+        alert(id);
+    }
+
+    return ( 
+        <li> 
+            <span> 
+                {item} ( {quantity} ) 
+            </span> 
+            <button onClick={handleDelete}>Delete</button>  
+            <button onclick={handleEdit}>Edit</button>       
+    </li> 
+    ); 
+} 
+
+ 
+export default function ShoppingList({ items, deleteItem }) { 
+    const ItemsJsx = items.map(listItem =>  
+        <ShoppingItem  
+            key={listItem.id}  
+            id={listItem.id} 
+            item={listItem.item}  
+            quantity={listItem.quantity} 
+            deleteItem={deleteItem} /> 
+    ); 
+ 
+    return <ul>{ItemsJsx}</ul>; 
+} 
