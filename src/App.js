c96128b3882e4c2673bd3bc0a96c8eb62593280f
Allison Lamb <allison.lamb624@gmail.com>
2023-12-11 21:24:09 -0500

    app.js adjustments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">App.js</div>
-  );
-}
-
-export default App;
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+        <h1>Shopping List</h1> 
+      </header> 
+      <main></main> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
